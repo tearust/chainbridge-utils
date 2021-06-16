c2f870d988fa955a7aa4c910690dc93759d00dcb
Yan Mingzhi <realraindust@gmail.com>
2021-06-16 14:43:58 +0800

    update gsrpc version

--- crypto/sr25519/sr25519.go
+++ crypto/sr25519/sr25519.go
@@ -7,8 +7,8 @@ import (
 	"crypto/rand"
 
 	"github.com/ChainSafe/chainbridge-utils/crypto"
-	"github.com/centrifuge/go-substrate-rpc-client/signature"
-	"github.com/centrifuge/go-substrate-rpc-client/types"
+	"github.com/centrifuge/go-substrate-rpc-client/v3/signature"
+	"github.com/centrifuge/go-substrate-rpc-client/v3/types"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
